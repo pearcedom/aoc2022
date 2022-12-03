00e69e7f96b9d1cf0c440b2e1a7e36afa3f2a556
Dominic Pearce <dominic.pearce@fiosgenomics.com>
2022-12-03 08:33:58 +0000

    simplify

--- src/day03.py
+++ src/day03.py
@@ -2,11 +2,7 @@
 
 from collections import Counter
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet_scores = {}
-for (i, j) in enumerate(alphabet):
-    alphabet_scores[j] = i+1
-    alphabet_scores[j.upper()] = i+27
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 with open("input/day03.txt") as f:
     x = f.read().split()
@@ -15,11 +11,11 @@ with open("input/day03.txt") as f:
     for i in x:
         mid = len(i) // 2 
         common = set(i[:mid]) & set(i[mid:])
-        res += alphabet_scores[list(common)[0]]
+        res += alphabet.index(list(common)[0]) + 1
     print("Part 1:", res)
 
     res = 0
     for i in range(0, len(x), 3):
         common = set(x[i]) & set(x[i+1]) & set(x[i+2])
-        res += alphabet_scores[list(common)[0]]
+        res += alphabet.index(list(common)[0]) + 1
     print("Part 2:", res)
