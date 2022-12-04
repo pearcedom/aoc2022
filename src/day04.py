579a43bf42f3a1682b663f96715f3ee7d90fe16f
Dominic Pearce <dominic.pearce@fiosgenomics.com>
2022-12-04 08:56:56 +0000

    use <= for contains

--- src/day04.py
+++ src/day04.py
@@ -4,5 +4,5 @@ with open("input/day04.txt") as f:
     x = [[[int(k) for k in j.split("-")] for j in i.split(",")] for i in f.read().split()]
     x = [[set(range(j[0], j[1]+1)) for j in i] for i in x]
 
-print("Part1:", sum(i[0].issubset(i[1]) or i[1].issubset(i[0]) for i in x))
+print("Part1:", sum(i[0] <= i[1] or i[1] <= i[0] for i in x))
 print("Part2:", sum(bool(i[0] & i[1]) for i in x))
